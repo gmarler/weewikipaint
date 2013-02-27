85aef433295ec433cf71a21e9a7a0c8465d92758
Gordon Marler <gmarler@gmarler.com>
2013-02-27 08:33:26 -0500

    First running test

---
+++ src/server/_server_test.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2013 gmarler.com. All rights reserved. See LICENSE.txt for details.
+"use strict";
+
+exports.testNothing = function(test) {
+    test.ok(false, "hello");
+    test.done();
+};
