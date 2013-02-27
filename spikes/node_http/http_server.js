8b8186951db9e1954169d442292bc8eaeee84fb9
Gordon Marler <gmarler@gmarler.com>
2013-02-27 12:44:06 -0500

    Add node HTTP server spike

---
+++ spikes/node_http/http_server.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2013 gmarler.com. All rights reserved. See LICENSE.txt for details.
+
+// This is a simple spike of Node's HTTP module.  The goal is to show how to
+// serve a very simple HTML page using Node.
+// It's not robust, and it reflects a very basic understanding of node;
+// use it as a starting point, not a production-quality example.
+
+var http = require("http");
+
+var server = http.createServer();
+
+server.on("request", function(request, response) {
+    console.log("Received request");
+
+    var body = "<html><head><title>Node HTTP Spike</title></head>" +
+        "<body><p>This is a spike of Node's HTTP server.</p></body></html>";
+
+    // concise way...
+    response.end(body);
+});
+
+server.listen(8080);
+
+console.log("Server started");
