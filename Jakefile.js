e07b6015ce1962d57821b29fbf3f7d28bd9f20f0
Gordon Marler <gmarler@gmarler.com>
2013-03-06 09:15:35 -0500

    extra newline

--- Jakefile.js
+++ Jakefile.js
@@ -63,6 +63,7 @@
             stdout += chunk;
         });
         process.on("cmdEnd", function() {
+            console.log();
             callback(stdout);
         });
         process.run();
