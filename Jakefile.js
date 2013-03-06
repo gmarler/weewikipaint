584e8bc3486823b476c7c36a035ecb0acfbe1c53
Gordon Marler <gmarler@gmarler.com>
2013-03-06 09:17:02 -0500

    eliminate need for embedded newline at end of Node version

--- Jakefile.js
+++ Jakefile.js
@@ -46,10 +46,10 @@
 
 //  desc("Ensure correct version of Node is present");
     task("node", [], function() {
-        var NODE_VERSION = "v0.8.4\n";  // Note appended newline
+        var NODE_VERSION = "v0.8.4";
 
         sh("node --version", function(stdout) {
-            if (stdout !== NODE_VERSION) fail("Incorrect node version. Expected " + NODE_VERSION);
+            if (stdout.trim() !== NODE_VERSION) fail("Incorrect node version. Expected " + NODE_VERSION);
             complete();
         });
     }, { async: true});
