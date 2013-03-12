484cbddf812e1ce34fb427290053d65de14766e7
Gordon Marler <gmarler@gmarler.com>
2013-03-12 00:02:08 -0400

    completion of file serving methods and add teardown method

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,55 +4,59 @@
 var server = require("./server.js");
 var http   = require("http");
 var fs = require("fs");
+var assert = require("assert");
 
+var TEST_FILE = "generated/test/test.html";
 
-exports.test_serverReturnsHelloWorld = function(test) {
-    server.start(8080);
+exports.tearDown = function(done) {
+    if (fs.existsSync(TEST_FILE)) {
+        fs.unlinkSync(TEST_FILE);
+        assert.ok(!fs.existsSync(TEST_FILE), "could not delete test file: [" + TEST_FILE + "]");
+    }
+    done();
+};
+
+exports.test_serverServesAFile = function(test) {
+    var testDir = "generated/test";
+    var testData = "This is served from a file";
+
+    fs.writeFileSync(TEST_FILE, testData);
+    server.start(TEST_FILE, 8080);
     var request = http.get("http://localhost:8080");
-    request.on("response", function(response) {
+    request.on("response", function (response) {
         var receivedData = false;
         response.setEncoding("utf8"); // To ensure chunk is a string
 
-        test.equals(200 , response.statusCode,"status code");
-        response.on("data", function(chunk) {
+        test.equals(200, response.statusCode, "status code");
+        response.on("data", function (chunk) {
             receivedData = true;
-            test.equals("Hello World", chunk, "response text");
+            test.equals(testData, chunk, "response text");
         });
-        response.on("end", function() {
+        response.on("end", function () {
             test.ok(receivedData, "should have received response data");
-            server.stop(function() {
+            server.stop(function () {
                 test.done();
             });
         });
     });
 };
 
-exports.test_serverServesAFile = function(test) {
-    var testDir = "generated/test";
-    var testFile = testDir + "/test.html";
-
-    try {
-        fs.writeFileSync(testFile, "Hello World");
-        test.done();
-    } finally {
-        fs.unlinkSync(testFile);
-        test.ok(!fs.existsSync(testFile), "file should have been deleted");
-    }
-
-
-
-    // TODO
-};
-
-exports.test_serverRequiresPortNumber = function(test) {
+exports.test_serverRequiresFileToServe = function(test) {
     test.throws(function() {
         server.start();
     });
     test.done();
 };
 
+exports.test_serverRequiresPortNumber = function(test) {
+    test.throws(function() {
+        server.start(TEST_FILE);
+    });
+    test.done();
+};
+
 exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
-    server.start(8080);
+    server.start(TEST_FILE, 8080);
     server.stop(function() {
         test.done();
     });
--- src/server/server.js
+++ src/server/server.js
@@ -2,18 +2,20 @@
 "use strict";
 
 var http = require("http");
+var fs = require("fs");
 var server;
 
 
-exports.start = function(portNumber) {
+exports.start = function(htmlFileToServe, portNumber) {
     if (!portNumber) throw new Error("port number is required");
 
     server = http.createServer();
 
     server.on("request", function(request, response) {
-        // response.statusCode = 404;
-        // Don't forget to end the response
-        response.end("Hello World");
+        fs.readFile(htmlFileToServe, function (err, data) {
+            if (err) throw err;      // TODO: fix me
+            response.end(data);
+        });
     });
 
     server.listen(portNumber);
